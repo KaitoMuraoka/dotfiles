1a2e5c9cc7c432a9e62cbcd9f027a48334a2264f
KaitoMuraoka <kaitoboshi@icloud.com>
2025-09-09 10:37:11 +0900

    npm をインストールし忘れたので、再度mason-lspを設定

--- editor/.config/nvim/lua/plugins/lsp/mason.lua
+++ editor/.config/nvim/lua/plugins/lsp/mason.lua
@@ -25,15 +25,29 @@ return {
     })
 
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
         "lua_ls",
-      },
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+        "eslint",
+      }
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "stylua"
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
       },
     })
   end,
--- editor/.config/nvim/lua/plugins/nvim-cmp.lua
+++ editor/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -41,6 +41,8 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer"},
         { name = "path" },
       }, {
         { name = "buffer" },
